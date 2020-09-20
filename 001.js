51e4e1ba31f2289821d54d277abbbe83d38f7b60
YongXun <53288723+YongXun@users.noreply.github.com>
2020-09-20 20:18:01 +0800

    翻转链表

--- 001.js
+++ 001.js
@@ -11,7 +11,7 @@
 
 // 来源：力扣（LeetCode）
 // 链接：https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof
-// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处
 
 var reverseList = function(head) {
     //虚拟节点
