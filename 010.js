246af5f27f084c1fc1fc62abff30fbed3715d5e8
yongxun <13623048317@163.com>
2020-10-23 18:44:25 +0800

    最大子序和

--- 010.js
+++ 010.js
@@ -12,7 +12,7 @@ let arr = [-2,1,-3,4,-1,2,1,-5,4];
 
 var maxSubArray = function(nums) {
     //若数组长度小于等于1，直接返回原数组。
-    if(nums.length <= 1){return nums;}
+    if(nums.length <= 1){return nums[0];}
     //遍历节点时，包含该节点的情况下能够达到的最大值。
     let maxHere= nums[0];
     //遍历节点时，遍历到一个节点以及之前所能达到的最大值。
