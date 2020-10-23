709cf81158ab78dd45dc8241e3e8877824b083b3
yongxun <13623048317@163.com>
2020-10-23 18:51:07 +0800

    最大子序和

--- 010.js
+++ 010.js
@@ -8,20 +8,22 @@
 // 输出: 6
 // 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
 
-let arr = [-2,1,-3,4,-1,2,1,-5,4];
+let arr = [1,2];
 
-var maxSubArray = function(nums) {
-    //若数组长度小于等于1，直接返回原数组。
-    if(nums.length <= 1){return nums[0];}
-    //遍历节点时，包含该节点的情况下能够达到的最大值。
-    let maxHere= nums[0];
-    //遍历节点时，遍历到一个节点以及之前所能达到的最大值。
-    let maxSoFar = nums[0];
-    for(const num of nums){
-        maxHere = Math.max(num,maxHere+num);
+const maxSubArray = (arr) => {
+    if(arr.length <= 1){
+        return arr[0];
+    }
+
+    let maxHere = arr[0];
+
+    let maxSoFar = arr[0];
+
+    for(let i = 1;i < arr.length;i++){
+        maxHere = Math.max(arr[i],maxHere+arr[i]);
         maxSoFar = Math.max(maxSoFar,maxHere);
     }
-    return maxSoFar;
-};
 
-console.log(maxSubArray(arr))
+    return maxSoFar;
+}
+
